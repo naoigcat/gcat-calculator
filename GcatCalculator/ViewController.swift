bb6a166b2ee13523eb7b13cc48b5711bef14f679
naoigcat <17925623+naoigcat@users.noreply.github.com>
2019-02-06 05:29:19 +0900

    Create project

---
+++ GcatCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GcatCalculator
+//
+//  Created by naoigcat on 2019/02/06.
+//  Copyright © 2019 naoigcat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
